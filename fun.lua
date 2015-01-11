dad35c01d649bdaee72344d71eaaa6ad4fccf9b3
José manuel Barroso Galindo <theypsilon@gmail.com>
2015-01-11 22:39:20 +0700

    In some lua versions, operator already exists

--- fun.lua
+++ fun.lua
@@ -1033,50 +1033,53 @@ exports.chain = chain
 -- Operators
 --------------------------------------------------------------------------------
 
-operator = {
-    ----------------------------------------------------------------------------
-    -- Comparison operators
-    ----------------------------------------------------------------------------
-    lt  = function(a, b) return a < b end,
-    le  = function(a, b) return a <= b end,
-    eq  = function(a, b) return a == b end,
-    ne  = function(a, b) return a ~= b end,
-    ge  = function(a, b) return a >= b end,
-    gt  = function(a, b) return a > b end,
+if not operator then
+    operator = {
+        ----------------------------------------------------------------------------
+        -- Comparison operators
+        ----------------------------------------------------------------------------
+        lt  = function(a, b) return a < b end,
+        le  = function(a, b) return a <= b end,
+        eq  = function(a, b) return a == b end,
+        ne  = function(a, b) return a ~= b end,
+        ge  = function(a, b) return a >= b end,
+        gt  = function(a, b) return a > b end,
 
-    ----------------------------------------------------------------------------
-    -- Arithmetic operators
-    ----------------------------------------------------------------------------
-    add = function(a, b) return a + b end,
-    div = function(a, b) return a / b end,
-    floordiv = function(a, b) return math.floor(a/b) end,
-    intdiv = function(a, b)
-        local q = a / b
-        if a >= 0 then return math.floor(q) else return math.ceil(q) end
-    end,
-    mod = function(a, b) return a % b end,
-    mul = function(a, b) return a * b end,
-    neq = function(a) return -a end,
-    unm = function(a) return -a end, -- an alias
-    pow = function(a, b) return a ^ b end,
-    sub = function(a, b) return a - b end,
-    truediv = function(a, b) return a / b end,
+        ----------------------------------------------------------------------------
+        -- Arithmetic operators
+        ----------------------------------------------------------------------------
+        add = function(a, b) return a + b end,
+        div = function(a, b) return a / b end,
+        floordiv = function(a, b) return math.floor(a/b) end,
+        intdiv = function(a, b)
+            local q = a / b
+            if a >= 0 then return math.floor(q) else return math.ceil(q) end
+        end,
+        mod = function(a, b) return a % b end,
+        mul = function(a, b) return a * b end,
+        neq = function(a) return -a end,
+        unm = function(a) return -a end, -- an alias
+        pow = function(a, b) return a ^ b end,
+        sub = function(a, b) return a - b end,
+        truediv = function(a, b) return a / b end,
 
-    ----------------------------------------------------------------------------
-    -- String operators
-    ----------------------------------------------------------------------------
-    concat = function(a, b) return a..b end,
-    len = function(a) return #a end,
-    length = function(a) return #a end, -- an alias
+        ----------------------------------------------------------------------------
+        -- String operators
+        ----------------------------------------------------------------------------
+        concat = function(a, b) return a..b end,
+        len = function(a) return #a end,
+        length = function(a) return #a end, -- an alias
 
-    ----------------------------------------------------------------------------
-    -- Logical operators
-    ----------------------------------------------------------------------------
-    land = function(a, b) return a and b end,
-    lor = function(a, b) return a or b end,
-    lnot = function(a) return not a end,
-    truth = function(a) return not not a end,
-}
+        ----------------------------------------------------------------------------
+        -- Logical operators
+        ----------------------------------------------------------------------------
+        land = function(a, b) return a and b end,
+        lor = function(a, b) return a or b end,
+        lnot = function(a) return not a end,
+        truth = function(a) return not not a end,
+    }
+end
+
 exports.operator = operator
 methods.operator = operator
 exports.op = operator
